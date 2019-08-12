85c7279da4d7c0a714b590fbd0b0418cf6487e40
diego.meneses <diego.meneses@pragma.com.co>
2019-08-12 15:44:15 -0500

    organizing the project and creating a more real api

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 const { buildSchema } = require('graphql');
 const express = require('express');
 const gqlMiddleware = require('express-graphql');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const resolvers = require('./lib/resolvers'); // Configure resolvers
 
 const app = express();
 const port = process.env.port || 3000;
 
 
 // define schema
-const schema = buildSchema(`
-  type Query {
-    hello: String,
-    bye: String
-  }
-`);
-
-// Configure resolvers
-const resolvers = {
-  hello: () => 'Hello world',
-  bye: () => 'Good bye',
-};
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8',
+  ),
+);
 
 app.use('/api', gqlMiddleware({
   schema,

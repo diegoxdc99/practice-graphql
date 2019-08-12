88ace26b1124b8224192058265b1861729208ad4
diego.meneses <diego.meneses@pragma.com.co>
2019-08-12 15:14:00 -0500

    Create file to linting code, configure npm task, update docs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# practice-graphql
+# Practice-graphql
+
 This repository is the practice of the platzi course related to graphql
+
+## **Command to generate the project:**
+Create a mit license, create a gitignore file for node and initialize the project with NPM
+**command: **npx license mit > LICENSE && npx gitignore node && npm init -y
+
+
+## Lint
+I used Eslint to guarantee the programming style
+**Command:** eslint --init
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-'use strict'
+const { buildSchema } = require('graphql');
+const express = require('express');
+const gqlMiddleware = require('express-graphql');
 
-const { graphql, buildSchema } = require('graphql');
+const app = express();
+const port = process.env.port || 3000;
 
 
-//define schema
+// define schema
 const schema = buildSchema(`
   type Query {
     hello: String,
@@ -11,14 +14,24 @@ const schema = buildSchema(`
   }
 `);
 
-//Configure resolvers
+// Configure resolvers
 const resolvers = {
   hello: () => 'Hello world',
-  bye: () => 'Good bye'
-}
+  bye: () => 'Good bye',
+};
+
+app.use('/api', gqlMiddleware({
+  schema,
+  rootValue: resolvers,
+  graphiql: true, // graphql development environment (browser)
+}));
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`);
+});
 
 // Execute query hello
 // I can choose the variables that I need through the query
-graphql(schema, '{ hello, bye }', resolvers).then((data) => {
-  console.log(data);
-})
+// graphql(schema, '{ hello, bye }', resolvers).then((data) => {
+//   console.log(data);
+// });
